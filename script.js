2761f13aacfeb31db48fd7b63f94f83927924764
Lemon Hamster <linoge67@yandex.ru>
2024-02-13 19:55:44 +0600

    jirabothtml

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var mySelectCause = document.getElementById('mySelectCause');
+    var svarInput = document.getElementById('svar');
+    var mySelectChange = document.getElementById('mySelectChange');
+    var amText = document.getElementById('am_text');
+  
+    function handleSelectChange(selectElement, targetElement, targetValue) {
+      selectElement.addEventListener('change', function() {
+        if (selectElement.value === targetValue) {
+          targetElement.style.display = 'block';
+        } else {
+          targetElement.style.display = 'none';
+        }
+      });
+  
+      // Проверка при загрузке страницы
+      if (selectElement.value === targetValue) {
+        targetElement.style.display = 'block';
+      } else {
+        targetElement.style.display = 'none';
+      }
+    }
+  
+    // Обработка для mySelectCause и svarInput
+    handleSelectChange(mySelectCause, svarInput, 'mcode');
+  
+    // Обработка для mySelectChange и amText
+    handleSelectChange(mySelectChange, amText, 'am');
+  });
+  
