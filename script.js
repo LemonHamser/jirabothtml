6f2edcdfbc0d13fcddb8985ffe459a0fc4f65cf9
Lemon Hamster <linoge67@yandex.ru>
2024-02-14 13:03:02 +0600

    рабочий кусок п 1

--- script.js
+++ script.js
@@ -1,30 +1,53 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var mySelectCause = document.getElementById('mySelectCause');
-    var svarInput = document.getElementById('svar');
-    var mySelectChange = document.getElementById('mySelectChange');
-    var amText = document.getElementById('am_text');
-  
-    function handleSelectChange(selectElement, targetElement, targetValue) {
-      selectElement.addEventListener('change', function() {
+  var mySelectCause = document.getElementById('mySelectCause');
+  var svarInput = document.getElementById('svar');
+  var mySelectChange = document.getElementById('mySelectChange');
+  var amText = document.getElementById('am_text');
+  var incProviders = document.getElementById('incProviders');
+  var changeIncProviders = document.getElementById('changeIncProviders');
+
+  function handleSelectChange(selectElement, targetElement, targetValue) {
+    selectElement.addEventListener('change', function() {
+      if (Array.isArray(targetValue)) {
+        if (targetValue.includes(selectElement.value)) {
+          targetElement.style.display = 'inline-block';
+        } else {
+          targetElement.style.display = 'none';
+        }
+      } else {
         if (selectElement.value === targetValue) {
           targetElement.style.display = 'inline-block';
         } else {
           targetElement.style.display = 'none';
         }
-      });
-  
-      // Проверка при загрузке страницы
+      }
+    });
+
+    // Проверка при загрузке страницы
+    if (Array.isArray(targetValue)) {
+      if (targetValue.includes(selectElement.value)) {
+        targetElement.style.display = 'inline-block';
+      } else {
+        targetElement.style.display = 'none';
+      }
+    } else {
       if (selectElement.value === targetValue) {
         targetElement.style.display = 'inline-block';
       } else {
         targetElement.style.display = 'none';
       }
     }
-  
-    // Обработка для mySelectCause и svarInput
-    handleSelectChange(mySelectCause, svarInput, 'mcode');
-  
-    // Обработка для mySelectChange и amText
-    handleSelectChange(mySelectChange, amText, 'am');
-  });
-  
+  }
+
+  // Обработка для mySelectCause и svarInput
+  handleSelectChange(mySelectCause, svarInput, 'mcode');
+
+  // Обработка для mySelectChange и amText
+  handleSelectChange(mySelectChange, amText, 'am');
+
+  // Обработка для mySelectCause и incProviders
+  handleSelectChange(mySelectCause, incProviders, ['sp', 'tr', 'ts']);
+
+  // Обработка для mySelectChange и changeIncProviders
+  handleSelectChange(mySelectChange, changeIncProviders, ['otme', 'izka']);
+});
